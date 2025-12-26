80adf7f2e48d657c147eb831c9cc9f7cdd260ba6
Jorge Loch <jorgembloch2001@gmail.com>
2025-12-26 16:36:24 -0300

    chore: removing unecessary spaces

--- day_02/part_1/main.go
+++ day_02/part_1/main.go
@@ -12,14 +12,11 @@ import (
 func compute_values(start uint64, end uint64) uint64 {
 	n_digits_start := uint64(math.Floor(math.Log10(float64(start))) + 1)
 	n_digits := uint64(math.Floor(float64(n_digits_start) / 2))
-
 	prefix := uint64(math.Floor(float64(start) / math.Pow(10, float64(n_digits_start-n_digits))))
-
 	counter := uint64(0)
 
 	for {
 		n_digits_prefix := uint64(math.Floor(math.Log10(float64(prefix))) + 1)
-
 		pattern := prefix * (uint64(math.Pow(10, float64(n_digits_prefix))) + 1)
 
 		if pattern > end {
